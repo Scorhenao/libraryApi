21f30131f8fa432eed0ab9cea06384b8e2e5e639
Scorhenao <samcorhenao@gmail.com>
2024-09-27 18:08:21 -0500

    feat: add createBook service with author existence check commented out

--- library-api/src/services/book/create-book/create-book.service.ts
+++ library-api/src/services/book/create-book/create-book.service.ts
@@ -3,15 +3,28 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateBookDto } from './dto/create-book.dto';
 import { BookDocument, BookEntity } from 'src/entities/book.schema';
+//import { AuthorService } from '../author/author.service'; // Asegúrate de que la ruta sea correcta
 
 @Injectable()
 export class CreateBookService {
   constructor(
     @InjectModel(BookEntity.name) private bookModel: Model<BookDocument>,
+    //private authorService: AuthorService, // Inyecta el servicio de autores
   ) {}
 
   async create(createBookDto: CreateBookDto): Promise<BookDocument> {
-    const createdBook = new this.bookModel(createBookDto);
+    // Verifica si el autor existe
+    //const authorExists = await this.authorService.findById(
+    //createBookDto.author,
+    //);
+    //if (!authorExists) {
+    //throw new BadRequestException('El autor no existe.');
+    //}
+
+    const createdBook = new this.bookModel({
+      ...createBookDto,
+      publicatedAt: new Date(createBookDto.publicatedAt), // Asegúrate de que este valor sea válido
+    });
     return createdBook.save();
   }
 }
