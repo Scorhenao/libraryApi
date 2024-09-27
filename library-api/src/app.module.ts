21ea5a02dee1708b8ca1abb6b453529540d9eca8
Scorhenao <samcorhenao@gmail.com>
2024-09-27 17:11:39 -0500

    feat(app): add BooksModule and configure ConfigModule as global

--- library-api/src/app.module.ts
+++ library-api/src/app.module.ts
@@ -1,11 +1,19 @@
 import { ConfigModule } from '@nestjs/config';
 import { CommonModule } from './common/commo.module';
 import { Module } from '@nestjs/common';
+import { BooksModule } from './services/book/books.module';
+import databaseConfig from './config/database.config';
+import { ConfigurationModule } from './config/config.module';
 
 @Module({
   imports: [
-    ConfigModule, // Solo importa ConfigModule una vez
+    ConfigModule.forRoot({
+      load: [databaseConfig],
+      isGlobal: true,
+    }),
+    ConfigurationModule,
     CommonModule,
+    BooksModule,
   ],
 })
 export class AppModule {}
