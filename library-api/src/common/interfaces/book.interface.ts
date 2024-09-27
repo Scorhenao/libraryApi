5ec7be2539bf21a9c0b95499eb8d6299eeffe42f
Scorhenao <samcorhenao@gmail.com>
2024-09-27 15:44:35 -0500

    fix: update author property to be an object in BookEntity and Book interface

--- library-api/src/common/interfaces/book.interface.ts
+++ library-api/src/common/interfaces/book.interface.ts
@@ -1,6 +1,8 @@
+import { Author } from 'src/entities/autor.schema';
+
 export interface Book {
   titulo: string;
-  author: string[];
+  author: Author;
   publicatedAt: string;
   genre: string;
 }
--- library-api/src/entities/book.schema.ts
+++ library-api/src/entities/book.schema.ts
@@ -1,15 +1,21 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { HydratedDocument } from 'mongoose';
+import { Author } from './autor.schema'; // Asegúrate de que la ruta sea correcta
+import { Book } from 'src/common/interfaces';
 
 export type BookDocument = HydratedDocument<Book>;
 
 @Schema()
-export class BookEntity implements BookInterface {
+export class BookEntity implements Book {
   @Prop()
   titulo: string;
 
-  @Prop({ required: true, type: mongoose.Schema.Types.ObjectId, ref: 'Author' })
-  author: Author;
+  @Prop({
+    required: true,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Author', // Referencia a Author
+  })
+  author: Author; // Cambiar a Author
 
   @Prop()
   publicatedAt: string;
@@ -18,4 +24,4 @@ export class BookEntity implements BookInterface {
   genre: string;
 }
 
-export const BookSchema = SchemaFactory.createForClass(Book);
+export const BookSchema = SchemaFactory.createForClass(BookEntity);
