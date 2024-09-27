5f6c468c1efe94d5bc080d2ac26e664007cfe286
Scorhenao <samcorhenao@gmail.com>
2024-09-27 17:14:32 -0500

    feat(schema): add uuid property to BookEntity

--- library-api/src/entities/book.schema.ts
+++ library-api/src/entities/book.schema.ts
@@ -2,11 +2,15 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { HydratedDocument } from 'mongoose';
 import { Author } from './autor.schema'; // Asegúrate de que la ruta sea correcta
 import { Book } from 'src/common/interfaces';
+import { v4 as uuidv4 } from 'uuid';
 
 export type BookDocument = HydratedDocument<Book>;
 
 @Schema()
 export class BookEntity implements Book {
+  @Prop({ default: uuidv4 }) // Genera un UUID por defecto
+  uuid: string;
+
   @Prop()
   titulo: string;
 
