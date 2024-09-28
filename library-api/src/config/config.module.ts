06e1e8e1bfb4f2bba5cacb2c8c0718772fb7dbd9
Scorhenao <samcorhenao@gmail.com>
2024-09-28 14:00:13 -0500

    fix change the uri and remove the parser and the unified topology from the config.module

--- library-api/src/config/config.module.ts
+++ library-api/src/config/config.module.ts
@@ -1,24 +1,19 @@
+import { ConfigModule, ConfigService } from '@nestjs/config'; // Corrige aquí
 import { Module } from '@nestjs/common';
-import {
-  ConfigModule as NestConfigModule,
-  ConfigService,
-} from '@nestjs/config'; // Renombrado
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    NestConfigModule.forRoot({
-      // Usa el nombre renombrado
+    ConfigModule.forRoot({
+      // Corrige aquí
       isGlobal: true,
     }),
     MongooseModule.forRootAsync({
       useFactory: (config: ConfigService) => ({
-        uri: config.get<string>('database.uri'),
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
+        uri: config.get<string>('MONGODB_URI'),
       }),
       inject: [ConfigService],
     }),
   ],
 })
-export class ConfigurationModule {} // Renombrado
+export class ConfigurationModule {}
