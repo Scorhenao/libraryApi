109fad405644d62a4dd48cd0050b12e1eb9914c8
Scorhenao <samcorhenao@gmail.com>
2024-09-28 22:52:07 -0500

    fix(docker): start with docker local to run in production

--- library-api/src/config/config.module.ts
+++ library-api/src/config/config.module.ts
@@ -1,17 +1,17 @@
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import databaseConfig from './database.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      load: [databaseConfig],
     }),
     MongooseModule.forRootAsync({
       useFactory: (config: ConfigService) => ({
         uri: config.get<string>('MONGODB_URI'),
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
       }),
       inject: [ConfigService],
     }),
